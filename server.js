fa35438afbdff171207f6c250c45f1f71b37c204
NgirfanulMangali <metalqwerty0@gmail.com>
2025-06-28 23:14:21 -0400

    learn create server

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello World');
+    if(req.url === '/'){
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1>Home Page</h1>');
+        res.end();
+    } else if(req.url === '/about'){
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1>About Page</h1>');
+        res.end();
+    } else if(req.url === '/contact'){
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1>Contact Page</h1>');
+        res.end();
+    } else {
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.write('<h1>404, Resource Not Found <a href="/">Go Back Home</a></h1>');
+        res.end();
+    }
 })
 
 server.listen(5000, () => {

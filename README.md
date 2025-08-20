38c587428f36ba7c1a8ad23e68e6e81b1164644d
NgirfanulMangali <metalqwerty0@gmail.com>
2025-08-19 23:39:54 -0700

    i have learned GET, POST, PUT and DELETE

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Hi there!
+# the Resource of the lesson
+
+- https://roadmap.sh/nodejs
+- handbook node js https://www.freecodecamp.org/news/get-started-with-nodejs/
+- how to create http https://www.digitalocean.com/community/tutorials/how-to-create-an-http-client-with-core-http-in-node-js#prerequisites
--- making-http/get.js
+++ making-http/get.js
@@ -1,10 +1,28 @@
-const axios = require('axios')
-axios
- .get('https://example.com/todos')
- .then((res) => {
- console.log(`statusCode: ${res.status}`)
- console.log(res)
- })
- .catch((error) => {
- console.error(error)
- })
+const https = require('https');
+
+let request = https.get('https://jsonplaceholder.typicode.com/users?_limit=2', (res) => { 
+    if (res.statusCode !== 200) {
+    console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+    res.resume();
+    return;
+    }
+
+
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('close', () => {
+    console.log('Retrieved all data');
+    console.log(JSON.parse(data));
+    });
+
+    request.on('error', (err) => {
+  console.error(`Encountered an error trying to make a request: ${err.message}`);
+    });
+
+});
+
+
